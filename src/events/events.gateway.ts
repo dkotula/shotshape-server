44a155f6df4afd33502020e9b601077dfd516ad6
dkotula <dkotula.wieliczka@gmail.com>
2021-11-21 11:02:08 +0100

    Added sending positions to all users

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,19 +1,30 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { OnGatewayConnection, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway()
-export class EventsGateway {
-  @WebSocketServer()
-  server: Server;
+export class EventsGateway implements OnGatewayConnection {
+  @WebSocketServer() server: Server;
+  private positions = [];
+  private logger: Logger = new Logger('EventsGateway');
 
   @SubscribeMessage('position')
-  handleEvent(@MessageBody() data: unknown): WsResponse<unknown> {
-    const event = 'position';
-    return { event, data };
+  handleMessage(client: Socket, data: unknown): void {
+    for (let player in this.positions) {
+      if (this.positions[player].id === client.id) {
+        this.positions[player].positions = data;
+        this.server.emit('position', this.positions[player]);
+        break;
+      }
+    }
   }
 
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+    this.positions.push({
+      id: client.id,
+      color: 'rgb(' + (Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256)) + ')',
+      positions: { x: 0, y: 0 },
+    });
   }
 }
